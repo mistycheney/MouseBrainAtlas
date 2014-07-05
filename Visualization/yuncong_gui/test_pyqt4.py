8e9716940eb9d647b1b012d6869f6bb0964a513a
Yuncong Chen <cyc3700@gmail.com>
2014-07-04 19:31:43 -0700

    move file reading out of GUI class

--- Visualization/yuncong_gui/test_pyqt4.py
+++ Visualization/yuncong_gui/test_pyqt4.py
@@ -2,7 +2,7 @@
 
 import sys
 import os
-import cv2
+from PIL import Image
 
 import numpy as np
 import matplotlib as mpl
@@ -27,24 +27,42 @@ else:
     from PyQt4.QtCore import *
     from PyQt4.QtGui import *
 
-output_dir = '/home/yuncong/BrainLocal/output'
-img_name = 'PMD1305_region0_reduce2_0244'
-result_name = '%s_param10'%img_name
+import argparse
 
-class AppForm(QMainWindow):
-    def __init__(self, parent=None):
+class PickByColorsGUI(QMainWindow):
+    def __init__(self, parent=None, img=None, segmentation=None, labellist=None, n_models=20):
         QMainWindow.__init__(self, parent)
-        #self.x, self.y = self.get_data()
-        self.get_data()
-        self.create_main_frame()
-        self.on_draw()
-        self.paint_label = None
-        self.pick_mode  =False
-        self.new_labels = []
+        
+        self.paint_label = None     # color of pen
+        self.pick_mode  = False     # True while you hold ctrl; that let's you pick a color from image
+        self.press = False          # related to pan (press and drag) vs. select (click)
+        self.base_scale = 1.3       # multiplication factor for zoom using scroll wheel
+
+        self.segmentation = segmentation
+
+        # convert labelmap (dict) to pixel values
+        self.labelmap_orig = -1*np.ones_like(segmentation, dtype=np.int)
+        self.labelmap_orig = labellist[segmentation]
+        self.labelmap_new = self.labelmap_orig.copy()
+
+        self.n_models = n_models
+        self.n_labels = self.n_models + 1
+
+        self.colors = [(1,1,1)] + [(random(),random(),random()) for i in xrange(30)] # be interactive
+        self.label_cmap = ListedColormap(self.colors, name='label_cmap')
+        self.img = img
+
+        self.create_main_frame() # construct the layout
+        self.initialize_canvas()  # initialize matplotlib plot
+
 
     def create_main_frame(self):
+        """
+        Declare the window layout and widgets
+        """
         self.main_frame = QWidget()
 
+        # matplotlib region (the brain image)
         self.fig = Figure((10.0, 5.0), dpi=100)
         self.fig.set_tight_layout(True)
         self.canvas = FigureCanvas(self.fig)
@@ -52,17 +70,21 @@ class AppForm(QMainWindow):
         self.canvas.setFocusPolicy(Qt.StrongFocus)
         self.canvas.setFocus()
 
-        # self.mpl_toolbar = NavigationToolbar(self.canvas, self.main_frame)
-
+        # callbacks
         self.canvas.mpl_connect('key_press_event', self.on_key_press)
         self.canvas.mpl_connect('key_release_event', self.on_key_release)
+        self.canvas.mpl_connect('scroll_event', self.zoom_fun)
+        self.canvas.mpl_connect('button_press_event', self.press_fun)
+        self.canvas.mpl_connect('button_release_event', self.release_fun)
+        self.canvas.mpl_connect('motion_notify_event', self.motion_fun)
 
-        self.colorButtons = [QPushButton('%d'%(i), self) for i in range(-1,self.n_labels-1)]
-        self.descEdits = [QLineEdit() for i in range(self.n_labels)]
+        # QT widgets definitions below
+
+        # existing color button/decription region
+        self.colorButtons = [QPushButton('%d'%(i), self) for i in range(-1, self.n_labels-1)]    # push buttons with colors
+        self.descEdits = [QLineEdit() for i in range(self.n_labels)]                            # corresponding decription fields
 
         self.color_box = QGridLayout()
-        # self.color_box.setSizeConstraint(QLayout.SetFixedSize)
-
         for i, (btn, edt) in enumerate(zip(self.colorButtons, self.descEdits)):
 
             r, g, b, a = self.label_cmap(i)
@@ -72,38 +94,48 @@ class AppForm(QMainWindow):
             self.color_box.addWidget(btn, i, 0)
             self.color_box.addWidget(edt, i, 1)
 
+        # new color button/decription region
         self.newcolor_box = QGridLayout()
 
+        # right box = exisitng color region + new color region
         right_box = QHBoxLayout()
         right_box.addLayout(self.color_box)
         right_box.addLayout(self.newcolor_box)
 
+        # left box = matplotlib canvas + buttons below
         left_box = QVBoxLayout()
         left_box.addWidget(self.canvas)
+
+        # "add new color" button
         newcolor_button = QPushButton('add new color', self)
         left_box.addWidget(newcolor_button)
+        # callback
         newcolor_button.clicked.connect(self.newcolor_button_clicked)
 
+        # quit button
         quit_button = QPushButton('quit', self)
         left_box.addWidget(quit_button)
         quit_button.clicked.connect(self.close)
 
+        # overall box = left box + right box
         all_box = QHBoxLayout()
         all_box.addLayout(left_box)
-        # vbox.addWidget(self.mpl_toolbar)
         all_box.addLayout(right_box)
+
         self.main_frame.setLayout(all_box)
         self.setCentralWidget(self.main_frame)
 
-        # self.statusBar()
+        help_message = 'Usage: Ctrl + Left Click to pick a color; Left Click to assign color to a superpixel; Scroll to zoom, Left Click + drag to pan'
+        self.setWindowTitle('PickByColorsGUI (%s)' % help_message)
 
-        self.setWindowTitle('Brain')
-
-        self.statusBar().showMessage('Ctrl + Left Click to pick a color; Left Click to assign color to a superpixel; Scroll to zoom, Left Click + drag to pan')
+        # self.statusBar().showMessage()
 
         self.show()
 
     def newcolor_button_clicked(self):
+        """
+        callback for "add new color"
+        """
 
         self.n_labels += 1
         self.paint_label = self.n_labels - 2
@@ -121,155 +153,153 @@ class AppForm(QMainWindow):
         self.newcolor_box.addWidget(btn, c, 0)
         self.newcolor_box.addWidget(edt, c, 1)
 
-
-    def get_data(self):
-        data_dir = os.path.join(output_dir, result_name+'_data')
-        # img_filename = os.path.join(data_dir, result_name + '_img_cropped.png')
-        img_filename = os.path.join(data_dir, result_name + '_segmentation.tif')
-        seg_filename = os.path.join(data_dir, result_name + '_segmentation.npy')
-        labelmap_filename = os.path.join(data_dir, result_name + '_labelmap.npy')        
-
-        self.segmentation = np.load(seg_filename)
-        self.labelmap = np.load(labelmap_filename)
-        self.n_models = 20
-        self.n_labels = self.n_models + 1
-
-        self.labelmap_new = self.labelmap.copy()
-
-        self.colors = [(1,1,1)] + [(random(),random(),random()) for i in xrange(30)]
-        self.label_cmap = ListedColormap(self.colors, name='label_cmap')
-        # self.label_cmap = mpl.colors.LinearSegmentedColormap.from_list()
-        self.img = cv2.imread(img_filename, 0)
-
-    def on_draw(self):
+    def initialize_canvas(self):
+        """
+        Initialize matplotlib canvas widget
+        """
         self.fig.clear()
         self.axes = self.fig.add_subplot(111)
         self.axes.axis('off')
-        self.zoom_factory(self.axes, base_scale=1.5)
-        #self.axes.plot(self.x, self.y, 'ro')
+
         self.axes.imshow(self.img, cmap=plt.cm.Greys_r,
                         aspect='equal')
-        self.label_layer = self.axes.imshow(self.labelmap + 1, cmap=self.label_cmap, alpha=0.2,
+        self.label_layer = self.axes.imshow(self.labelmap_orig + 1, cmap=self.label_cmap, alpha=0.2,
                         aspect='equal', norm=NoNorm())
         self.canvas.draw()
+
+
+    # CALLBACKS 
 
     def on_key_press(self, event):
         if event.key == 'control':
             self.pick_mode = True
             print 'pick mode on'
 
-        # implement the default mpl key press events described at
-        # http://matplotlib.org/users/navigation_toolbar.html#navigation-keyboard-shortcuts
-        # key_press_handler(event, self.canvas, self.mpl_toolbar)
-
     def on_key_release(self, event):
         if event.key == 'control':
             self.pick_mode = False
             print 'pick mode off'
+        
+    def zoom_fun(self, event):
+        # get the current x and y limits
+        cur_xlim = self.axes.get_xlim()
+        cur_ylim = self.axes.get_ylim()
+        # set the range
+        cur_xrange = (cur_xlim[1] - cur_xlim[0])*.5
+        cur_yrange = (cur_ylim[1] - cur_ylim[0])*.5
+        xdata = event.xdata # get event x location
+        ydata = event.ydata # get event y location
 
-    def zoom_factory(self, ax, base_scale = 2.):
-        self.press = False
+        left = xdata - cur_xlim[0]
+        right = cur_xlim[1] - xdata
+        up = ydata - cur_ylim[0]
+        down = cur_ylim[1] - ydata
 
-        def zoom_fun(event):
-            # get the current x and y limits
-            cur_xlim = ax.get_xlim()
-            cur_ylim = ax.get_ylim()
-            # set the range
-            cur_xrange = (cur_xlim[1] - cur_xlim[0])*.5
-            cur_yrange = (cur_ylim[1] - cur_ylim[0])*.5
-            xdata = event.xdata # get event x location
-            ydata = event.ydata # get event y location
-
-            left = xdata - cur_xlim[0]
-            right = cur_xlim[1] - xdata
-            up = ydata - cur_ylim[0]
-            down = cur_ylim[1] - ydata
-
-            if event.button == 'up':
-                # deal with zoom in
-                scale_factor = 1/base_scale
-            elif event.button == 'down':
-                # deal with zoom out
-                scale_factor = base_scale            
-                
-            # set new limits
-            ax.set_xlim([xdata - left*scale_factor,
-                         xdata + right*scale_factor])
-            ax.set_ylim([ydata - up*scale_factor,
-                         ydata + down*scale_factor])
-            ax.figure.canvas.draw() # force re-draw
-     
-        def press_fun(event):
-            self.press_x = event.xdata
-            self.press_y = event.ydata
-            self.press = True
-
-        def motion_fun(event):
-            self.moved = True
-            if self.press:
-                cur_xlim = ax.get_xlim()
-                cur_ylim = ax.get_ylim()
-                
-                offset_x = self.press_x - event.xdata
-                offset_y = self.press_y - event.ydata
-                
-                ax.set_xlim(cur_xlim + offset_x)
-                ax.set_ylim(cur_ylim + offset_y)
-                ax.figure.canvas.draw()
-
-        def release_fun(event):
-            self.press = False
+        if event.button == 'up':
+            # deal with zoom in
+            scale_factor = 1/self.base_scale
+        elif event.button == 'down':
+            # deal with zoom out
+            scale_factor = self.base_scale
             
-            # click without moving, means selection
-            if self.moved: self.moved = False
-            if event.xdata == self.press_x and event.ydata == self.press_y:
-                # self.axes.clear()
+        # set new limits
+        self.axes.set_xlim([xdata - left*scale_factor,
+                     xdata + right*scale_factor])
+        self.axes.set_ylim([ydata - up*scale_factor,
+                     ydata + down*scale_factor])
+        self.axes.figure.canvas.draw() # force re-draw
 
-                self.selected_sp = self.segmentation[int(event.ydata), int(event.xdata)]
-                self.selected_label = self.labelmap_new[int(event.ydata), int(event.xdata)]
-                
-                if self.pick_mode:
-                    self.paint_label = self.labelmap_new[int(event.ydata), int(event.xdata)]
+    def press_fun(self, event):
+        self.press_x = event.xdata
+        self.press_y = event.ydata
+        self.press = True
+
+    def motion_fun(self, event):
+        self.moved = True
+        if self.press:
+            cur_xlim = self.axes.get_xlim()
+            cur_ylim = self.axes.get_ylim()
+            
+            offset_x = self.press_x - event.xdata
+            offset_y = self.press_y - event.ydata
+            
+            self.axes.set_xlim(cur_xlim + offset_x)
+            self.axes.set_ylim(cur_ylim + offset_y)
+            self.axes.figure.canvas.draw()
+
+    def release_fun(self, event):
+        self.press = False
+        
+        # click without moving, means selection
+        if self.moved: self.moved = False
+        if event.xdata == self.press_x and event.ydata == self.press_y:
+            # self.axes.clear()
+
+            self.selected_sp = self.segmentation[int(event.ydata), int(event.xdata)]
+            self.selected_label = self.labelmap_new[int(event.ydata), int(event.xdata)]
+            
+            if self.pick_mode:
+                self.paint_label = self.labelmap_new[int(event.ydata), int(event.xdata)]
+            else:
+                if self.paint_label is None:
+                    self.statusBar().showMessage('superpixel %d, selected label = %d'%(self.selected_sp,
+                                self.selected_label))
                 else:
-                    if self.paint_label is None:
-                        self.statusBar().showMessage('superpixel %d, selected label = %d'%(self.selected_sp,
-                                    self.selected_label))
-                    else:
-                        self.labelmap_new[self.segmentation == self.selected_sp] = self.paint_label
+                    self.labelmap_new[self.segmentation == self.selected_sp] = self.paint_label
 
-                if self.paint_label is not None:
-                    self.statusBar().showMessage('superpixel %d, selected label = %d, paint label = %d'%(self.selected_sp,
-                                    self.selected_label,
-                                    self.paint_label))
+            if self.paint_label is not None:
+                self.statusBar().showMessage('superpixel %d, selected label = %d, paint label = %d'%(self.selected_sp,
+                                self.selected_label,
+                                self.paint_label))
 
-                curr_xlim = ax.get_xlim()
-                curr_ylim = ax.get_ylim()
+            curr_xlim = self.axes.get_xlim()
+            curr_ylim = self.axes.get_ylim()
 
-                self.label_layer.remove()
+            self.label_layer.remove()
 
-                self.label_layer = self.axes.imshow(self.labelmap_new + 1, cmap=self.label_cmap, alpha=.2,
-                        aspect='equal', norm=NoNorm())
+            self.label_layer = self.axes.imshow(self.labelmap_new + 1, cmap=self.label_cmap, alpha=.2,
+                    aspect='equal', norm=NoNorm())
 
-                self.axes.set_xlim(curr_xlim)
-                self.axes.set_ylim(curr_ylim)
-                self.axes.figure.canvas.draw() # force re-draw
-
-
-            # # click and drag, means pan
-            # else:
-                 # force re-draw
-
-        fig = ax.get_figure() # get the figure of interest
-        # attach the call back
-        fig.canvas.mpl_connect('scroll_event',zoom_fun)
-        fig.canvas.mpl_connect('button_press_event',press_fun)
-        fig.canvas.mpl_connect('button_release_event',release_fun)
-        fig.canvas.mpl_connect('motion_notify_event',motion_fun)
+            self.axes.set_xlim(curr_xlim)
+            self.axes.set_ylim(curr_ylim)
+            self.axes.figure.canvas.draw() # force re-draw
 
 def main():
+    parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description='GUI for coloring superpixels',
+    epilog="""
+    For example:
+    python %s PMD1305_region0_reduce2_0244_param10
+    """%(os.path.basename(sys.argv[0]), ))
+
+    parser.add_argument("result_name", type=str, help="name of the result")
+    parser.add_argument("-d", "--data_dir", type=str, help="result data directory (default: %(default)s)", default='.')
+    args = parser.parse_args()
+
+    data_dir = os.path.realpath(args.data_dir)
+
+    # The brain image with superpixel boundaries drawn on it
+    img_filename = os.path.join(data_dir, args.result_name + '_segmentation.tif')
+
+    # a matrix of labels indicating which superpixel a pixel belongs to 
+    # each label is an integer from 0 to n_superpixels.
+    # -1 means background, 0 to n_superpixel-1 corresponds to each superpixel
+    seg_filename = os.path.join(data_dir, args.result_name + '_segmentation.npy')
+    
+    # a list of labels indicating which model a suerpixel is associated with. 
+    # Each label is an integer from -1 to n_models-1.
+    # -1 means background, 0 to n_models-1 corresponds to each model
+    labellist_filename = os.path.join(data_dir, args.result_name + '_labellist.npy') 
+
+    img = np.array(Image.open(img_filename)).mean(axis=-1)
+    segmentation = np.load(seg_filename)
+    labellist = np.load(labellist_filename)
+
     app = QApplication(sys.argv)
-    form = AppForm()
-    form.show()
+    main_window = PickByColorsGUI(img=img, segmentation=segmentation, 
+                                    labellist=labellist, n_models=20)
+    main_window.show()
     app.exec_()
 
 if __name__ == "__main__":

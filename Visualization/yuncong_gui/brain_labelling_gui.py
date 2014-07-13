918a11550eb3118b9d7e662f8b1f83fd78b3929e
yoav freund <yfreund@ucsd.edu>
2014-07-13 16:36:56 -0700

    GUI works, now working on getting the significance based analysis to work interactively. Using ipython notebooks for that.

--- Visualization/yuncong_gui/brain_labelling_gui.py
+++ Visualization/yuncong_gui/brain_labelling_gui.py
@@ -1,9 +1,5 @@
-# from __future__ import print_function
-
 import sys
-import os
-from PIL import Image
-
+import datetime
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -21,16 +17,13 @@ from matplotlib.colors import ListedColormap, NoNorm, ColorConverter
 use_pyside = qt4_compat.QT_API == qt4_compat.QT_API_PYSIDE
 
 if use_pyside:
-    print 'Using PySide'
+    #print 'Using PySide'
     from PySide.QtCore import *
     from PySide.QtGui import *
 else:
-    print 'Using PyQt4'
+    #print 'Using PyQt4'
     from PyQt4.QtCore import *
     from PyQt4.QtGui import *
-
-import argparse
-import pickle
 
 class PickByColorsGUI(QMainWindow):
     def __init__(self, parent=None, recalc_callback=None, save_callback=None,\
@@ -42,34 +35,53 @@ class PickByColorsGUI(QMainWindow):
         save_callback: A callback function which is launched when the user clicks on the "save" button.
         img:          2D array containing the image.
         segmentation: 2D array containing the segment index for each pixel, generated using the super-pixel alg.
-        labeling:    A dictionary containing the mapping of super-pixels to colors (labellist) 
+        labeling:    A dictionary containing the mapping of super-pixels to colors (label_history) 
                       and ascii names for the colors.
         """
+        self.app = QApplication(sys.argv)
         QMainWindow.__init__(self, parent)
-        
-        self.paint_label = None     # color of pen
-        self.pick_mode  = False     # True while you hold ctrl; used to pick a color from the image
-        self.press = False          # related to pan (press and drag) vs. select (click)
-        self.base_scale = 1.3       # multiplication factor for zoom using scroll wheel
 
         self.segmentation = segmentation
+        self.n_superpixels=max(segmentation.flatten())+1
+        self.img = img
 
         self.recalc_callback=recalc_callback
         self.save_callback=save_callback
 
-        # convert labelmap (dict) to pixel values
-        self.labellist=labeling['labellist']
-        print 'shape of labellist',np.shape(self.labellist)
-        self.buttonTexts=labeling['oldnames']
+        # Read in the labellist history. In labeling we store an array with the history of 
+        # labelings that we can run through for a demo, perform undo, redo etc.
+        # The fields in labeling are:
+        # labellist: an array that maps segment (super-pixel) number to labels
+        # names: an array that contains the textual name associated with the label.
+        # label_history: The history of changes, together with full label-lists from time to time.
+        #         The fields in each elements of the list are:
+        #         name - name of user
+        #         time
+        #         full: Binary, true if all of the frame is saved, else is just the change
+        #         data: if full: array of labels (labellist), if not: a pair: (segment_no, label)
 
-        self.labelmap_orig = -1*np.ones_like(segmentation, dtype=np.int)
-        self.labelmap_orig = self.labellist[segmentation]
-        self.labelmap_new = self.labelmap_orig.copy()
+        if labeling == None:
+            print 'No labeling given'
+            self.labeling={}
+            self.labellist=-1*np.ones(self.n_superpixels)
+            self.labeling['labellist']=self.labellist
+            self.n_models=10
+            self.labeling['names']=['No Label']+['Label %2d'%i for i in range(self.n_models+1)]
+            self.labeling['label_history']=[]
+        else:
+            self.labeling=labeling
+            self.labellist=labeling['labellist']
+            self.n_models = max(10,np.max(self.labellist)+1)
 
-        self.n_models = max(self.labellist)+1
+        self.buttonTexts=self.labeling['names']
         self.n_labels = self.n_models + 1
+        print 'n_labels',self.n_labels
 
-        # Colors proposed by Green-Armytage 
+        self.labelmap = -1*np.ones_like(self.segmentation, dtype=np.int)
+        if self.n_labels>1:
+            self.labelmap = self.labellist[self.segmentation]
+
+        # A set of high-contrast Colors proposed by Green-Armytage 
         self.colors = [(255,255,255),
                        (240,163,255),(0,117,220),(153,63,0),(76,0,92),(25,25,25),(0,92,49),(43,206,72),
                        (255,204,153),(128,128,128),(148,255,181),(143,124,0),(157,204,0),(194,0,136),
@@ -78,11 +90,16 @@ class PickByColorsGUI(QMainWindow):
         for i in range(len(self.colors)):
             self.colors[i]=tuple([float(c)/255.0 for c in self.colors[i]])
         self.label_cmap = ListedColormap(self.colors, name='label_cmap')
-        self.img = img
 
-        self.create_main_frame() # construct the layout
-        self.initialize_canvas()  # initialize matplotlib plot
+        # initialize GUI variables
+        self.paint_label = -1        # color of pen
+        self.pick_mode  = False      # True while you hold ctrl; used to pick a color from the image
+        self.press = False           # related to pan (press and drag) vs. select (click)
+        self.base_scale = 1.05       # multiplication factor for zoom using scroll wheel
+        self.moved=False             # Flag indicating whether mouse moved while button pressed
 
+        self.create_main_frame()     # construct the layout
+        self.initialize_canvas()     # initialize matplotlib plot
 
     def create_main_frame(self):
         """
@@ -111,18 +128,32 @@ class PickByColorsGUI(QMainWindow):
         # QT widgets definitions and layout 
         ############################################
         # existing color button/decription region
-        self.colorButtons = [QPushButton('%d'%(i), self) for i in range(-1, self.n_labels-1)]    # push buttons with colors
+        self.colorButtons = []    # layout container for push buttons with colors
+        self.buttonGroup = QButtonGroup(self) # event handling container for same buttons. 
+
+
+        for i in range(-1, self.n_labels-1):
+            button=QPushButton('%d'%(i), self)
+            self.colorButtons.append(button)
+            self.buttonGroup.addButton(button)
+
+        self.buttonGroup.buttonClicked.connect(self.handleButtonClicked)
+        
+
         self.descEdits = [QLineEdit(QString(self.buttonTexts[i])) for i in range(self.n_labels)] # corresponding decription fields
         self.newDescEdits=[]
 
+        self.NameField=QLineEdit(QString('Your Name'))
+
         self.color_box = QGridLayout()
+        self.color_box.addWidget(self.NameField, 0, 1)
         for i, (btn, edt) in enumerate(zip(self.colorButtons, self.descEdits)):
             r, g, b, a = self.label_cmap(i)
             btn.setStyleSheet("background-color: rgba(%d, %d, %d, 20%%)"%(
                 int(r*255), int(g*255), int(b*255)))
             btn.setFixedSize(20, 20)
-            self.color_box.addWidget(btn, i, 0)
-            self.color_box.addWidget(edt, i, 1)
+            self.color_box.addWidget(btn, i+1, 0)
+            self.color_box.addWidget(edt, i+1, 1)
 
         # new color button/decription region
         self.newcolor_box = QGridLayout()
@@ -207,23 +238,25 @@ class PickByColorsGUI(QMainWindow):
         self.axes.axis('off')
 
         self.axes.imshow(self.img, cmap=plt.cm.Greys_r, aspect='equal')
-        self.label_layer = self.axes.imshow(self.labelmap_orig + 1,\
-                                            cmap=self.label_cmap, alpha=0.2,
-                                            aspect='equal', norm=NoNorm())
+
+        self.label_layer=None  # to avoid removing layer when it is not yet there
+        self.draw_colors()
+
         self.canvas.draw()
 
 
     # CALLBACKS 
 
+    def handleButtonClicked(self, button):
+        self.pick_color(int(button.text()))
+
     def on_key_press(self, event):
         if event.key == 'control':
             self.pick_mode = True
-            print 'pick mode on'
 
     def on_key_release(self, event):
         if event.key == 'control':
             self.pick_mode = False
-            print 'pick mode off'
         
     def zoom_fun(self, event):
         # get the current x and y limits
@@ -283,106 +316,66 @@ class PickByColorsGUI(QMainWindow):
         
         # click without moving, means selection
         if self.moved: self.moved = False
+
         if event.xdata == self.press_x and event.ydata == self.press_y:
             # self.axes.clear()
-
-            self.selected_sp = self.segmentation[int(event.ydata), int(event.xdata)]
-            self.selected_label = self.labelmap_new[int(event.ydata), int(event.xdata)]
-            
+            try:
+                selected_sp = self.segmentation[int(event.ydata), int(event.xdata)]
+                selected_label = self.labelmap[int(event.ydata), int(event.xdata)]
+            except:
+                return
             if self.pick_mode:
                 # Picking a color
-                self.paint_label = self.labelmap_new[int(event.ydata), int(event.xdata)]
+                self.pick_color(selected_label)
             else:
                 #Painting a color
-                if self.paint_label is None:
-                    self.statusBar().showMessage('superpixel %d, selected label = %d'%(self.selected_sp,
-                                self.selected_label))
-                else:
-                    self.labelmap_new[self.segmentation == self.selected_sp] = self.paint_label
-                    self.labellist[self.selected_sp]=self.paint_label
+                self.statusBar().showMessage('Paint color: superpixel %d, selected label = %d' % (selected_sp, self.paint_label))
+                self.change_seg_color(selected_sp)
+            self.draw_colors()
 
-            if self.paint_label is not None:
-                self.statusBar().showMessage('superpixel %d, selected label = %d, paint label = %d'%(self.selected_sp,
-                                self.selected_label,
-                                self.paint_label))
+    def pick_color(self,selected_label):
+        self.paint_label = selected_label
+        self.statusBar().showMessage('Choose label: selected label = %d' % self.paint_label)
 
-            curr_xlim = self.axes.get_xlim()
-            curr_ylim = self.axes.get_ylim()
+    def change_seg_color(self,selected_sp):
 
+        print 'self.paint_label',self.paint_label
+        self.labellist[selected_sp]=self.paint_label
+        self.labelmap = self.labellist[self.segmentation]
+
+        timestamp=str(datetime.datetime.now())
+        username=str(self.NameField.text())
+        self.labeling['label_history'].append({'name':username,
+                                               'time':timestamp,
+                                               'Full':False,
+                                               'data':(selected_sp,self.paint_label)
+                                           })
+        
+
+    def draw_colors(self):
+        if self.label_layer!=None:
             self.label_layer.remove()
 
-            self.label_layer = self.axes.imshow(self.labelmap_new + 1, cmap=self.label_cmap, alpha=.2,
-                    aspect='equal', norm=NoNorm())
+        import collections
 
-            self.axes.set_xlim(curr_xlim)
-            self.axes.set_ylim(curr_ylim)
-            self.axes.figure.canvas.draw() # force re-draw
-    
+        print 'draw_colors: values in labelmap are',set(self.labelmap.flatten())
+        self.label_layer = self.axes.imshow((self.labelmap + 1)/float(len(self.colors)), cmap=self.label_cmap, alpha=.2,
+                aspect='equal', norm=NoNorm())
+
+        #self.axes.set_xlim(cur_xlim)
+        #self.axes.set_ylim(cur_ylim)
+        self.axes.figure.canvas.draw() # force re-draw
+
     def get_labels(self):
-        A={}
-        A['labellist']=self.labellist
-        A['oldnames']=[str(q.text()) for q in self.descEdits]
-        A['newnames']=[str(q.text()) for q in self.newDescEdits]
-        return A
-
-class Interactive_Labeling:
-    def recalc_handler(self,event):
-        print 'recalc_handler',event
-        print self.main_window.get_labels()
-
-    def save_handler(self,event):
-        print 'save_handler'
-        state = self.main_window.get_labels()
-        print state
-        pickle.dump(state,open(self.labellist_filename,'w'))
-
-    def full_name(self,extension):
-        return os.path.join(self.data_dir, self.result_name + extension)
-
-    def main(self):
-        parser = argparse.ArgumentParser(
-            formatter_class=argparse.RawDescriptionHelpFormatter,
-            description='GUI for coloring superpixels',
-            epilog="""Example:
-            python %s PMD1305_region0_reduce2_0244_param_nissl324
-            """%(os.path.basename(sys.argv[0]), ))
-
-        parser.add_argument("result_name", type=str, help="name of the result")
-        parser.add_argument("-d", "--data_dir", type=str, help="result data directory (default: %(default)s)",\
-                            default='.')
-        args = parser.parse_args()
-        self.result_name=args.result_name
-        self.data_dir=args.data_dir
-        data_dir = os.path.realpath(args.data_dir)
-
-        # The brain image with superpixel boundaries drawn on it
-        self.img_filename = self.full_name('_segmentation.png')
-
-        # a matrix of labels indicating which superpixel a pixel belongs to 
-        # each label is an integer from 0 to n_superpixels.
-        # -1 means background, 0 to n_superpixel-1 corresponds to each superpixel
-        self.seg_filename = self.full_name('_segmentation.npy')
-
-        # a list of labels indicating which model a suerpixel is associated with. 
-        # Each label is an integer from -1 to n_models-1.
-        # -1 means background, 0 to n_models-1 corresponds to each model
-        self.labellist_filename = self.full_name('_labeling.pkl') 
-
-        img = np.array(Image.open(self.img_filename)).mean(axis=-1)
-        segmentation = np.load(self.seg_filename)
-
-        self.labeling = pickle.load(open(self.labellist_filename,'r'))
-
-        app = QApplication(sys.argv)
-        main_window = PickByColorsGUI(img=img, segmentation=segmentation,\
-                                      recalc_callback=self.recalc_handler,\
-                                      save_callback=self.save_handler,\
-                                      labeling=self.labeling)
-        main_window.show()
-        self.main_window=main_window
-        app.exec_()
-
-if __name__ == "__main__":
-    IL=Interactive_Labeling()
-    IL.main()
+        self.labeling['labeling']=self.labeling
+        self.labeling['names']=[str(self.descEdits[i].text()) for i in range(len(self.descEdits))]
+        timestamp=str(datetime.datetime.now())
+        username=str(self.NameField.text())
+        self.labeling['label_history'].append({'name':username,
+                                               'time':timestamp,
+                                               'Full':True,
+                                               'data':self.labeling
+                                           })
+        
+        return self.labeling
 

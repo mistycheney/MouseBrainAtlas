3e656f75879c3c13c86d9aaa45f75da43c346905
yuncong <cyc3700@gmail.com>
2014-10-08 13:30:37 -0700

    remove symbolic links, instead, set env variables in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 CSD181=/oasis/projects/nsf/csd181/yuncong
+
 export PYTHONPATH=$CSD181/opencv/release/lib/python2.7/site-packages:$PYTHONPATH
 export LD_LIBRARY_PATH=/opt/python/lib:$LD_LIBRARY_PATH:$CSD181/KDU74_Demo_Apps_for_Linux-x86-64_140513
 export PATH=/oasis/projects/nsf/csd181/yuncong/virtualenv-1.9.1/yuncongve/bin:$PATH:$CSD181/KDU74_Demo_Apps_for_Linux-x86-64_140513
 source /opt/intel/composer_xe_2013_sp1.2.144/bin/compilervars.sh intel64
+
+export GORDON_DATA_DIR=$CSD181/DavidData
+export LOCAL_DATA_DIR=/home/yuncong/BrainLocal/DavidData

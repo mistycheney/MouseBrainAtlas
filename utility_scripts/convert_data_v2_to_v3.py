8a56d5b53bdb5cfdede141a08178bbdd0e43ba2d
Yuncong Chen <cyc3700@gmail.com>
2014-10-20 05:22:39 -0700

    small fiz

--- utility_scripts/convert_data_v2_to_v3.py
+++ utility_scripts/convert_data_v2_to_v3.py
@@ -22,26 +22,28 @@ if not os.path.exists(new_data_dir):
 
 for path, folders, files in os.walk(old_data_dir):
 	all_segments = path.split('/')
-	nlevel = len(all_segments) - external_nlevel - 1
-	internal_segments = '/'.join(all_segments[-3:])
-	image_name = '_'.join(all_segments[-3:])
+	nlevel = len(all_segments) - external_nlevel
+	internal_segments = '/'.join(all_segments[external_nlevel:])
+	image_name = '_'.join(all_segments[external_nlevel:])
 	new_path = new_data_dir + '/' + internal_segments
-
-	# print path, new_path, folders
 
 	if nlevel == 3:
 		if not os.path.exists(new_path+'/labelings'):
 			os.makedirs(new_path+'/labelings')
 
 		for paramSet in folders: # paramSets
+
+
+			# print internal_segments
+			# print path, new_path, folders
 			# print paramSet
 
 			print 'cp', path+'/'+image_name+'.tif', new_path+'/'+image_name+'.tif'
 			shutil.copyfile(path+'/'+image_name+'.tif', new_path+'/'+image_name+'.tif')
 			
-			# os.makedirs(new_path+'/'+paramSet+'_pipelineResults')
-			shutil.copytree(path+'/'+paramSet+'/pipelineResults', new_path+'/'+paramSet+'_pipelineResults')
 			print 'cp', path+'/'+paramSet+'/pipelineResults', new_path+'/'+paramSet+'_pipelineResults'
+			shutil.copytree(path+'/'+paramSet+'/pipelineResults', new_path+'/'+paramSet+'_pipelineResults')
+			
 
 			for f in os.listdir(path+'/'+paramSet+'/labelings'):
 				new_labeling_name = '_'.join([x for x in f.split('_') if x != paramSet])

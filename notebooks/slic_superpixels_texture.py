5b66c2085dc177dc39eaea90646026c85c7ca96e
Yuncong Chen <cyc3700@gmail.com>
2014-11-26 15:22:19 -0800

    added texture slic

---
+++ notebooks/slic_superpixels_texture.py
@@ -0,0 +1,80 @@
+# coding=utf-8
+
+import collections as coll
+import numpy as np
+from scipy import ndimage
+import warnings
+
+from skimage.util import img_as_float, regular_grid
+from _slic import _slic_cython, _enforce_label_connectivity_cython
+from skimage.color import rgb2lab
+
+
+def slic(image, n_segments=100, compactness=10., max_iter=10, sigma=0,
+         spacing=None, multichannel=True, convert2lab=True,
+         enforce_connectivity=False, min_size_factor=0.5, max_size_factor=3):
+
+    image = img_as_float(image)
+    image = image[np.newaxis, ..., np.newaxis]
+    is_2d = True
+
+    if spacing is None:
+        spacing = np.ones(3)
+    elif isinstance(spacing, (list, tuple)):
+        spacing = np.array(spacing, dtype=np.double)
+
+    if not isinstance(sigma, coll.Iterable):
+        sigma = np.array([sigma, sigma, sigma], dtype=np.double)
+        sigma /= spacing.astype(np.double)
+    elif isinstance(sigma, (list, tuple)):
+        sigma = np.array(sigma, dtype=np.double)
+    if (sigma > 0).any():
+        # add zero smoothing for multichannel dimension
+        sigma = list(sigma) + [0]
+        image = ndimage.gaussian_filter(image, sigma)
+
+    if convert2lab and multichannel:
+        if image.shape[3] != 3:
+            raise ValueError("Lab colorspace conversion requires a RGB image.")
+        image = rgb2lab(image)
+
+    depth, height, width = image.shape[:3]
+
+    # initialize cluster centroids for desired number of segments
+    grid_z, grid_y, grid_x = np.mgrid[:depth, :height, :width]
+    slices = regular_grid(image.shape[:3], n_segments)
+    step_z, step_y, step_x = [int(s.step) for s in slices]
+    segments_z = grid_z[slices]
+    segments_y = grid_y[slices]
+    segments_x = grid_x[slices]
+
+    segments_color = np.zeros(segments_z.shape + (image.shape[3],))
+    segments = np.concatenate([segments_z[..., np.newaxis],
+                               segments_y[..., np.newaxis],
+                               segments_x[..., np.newaxis],
+                               segments_color],
+                              axis=-1).reshape(-1, 3 + image.shape[3])
+    segments = np.ascontiguousarray(segments)
+
+    # we do the scaling of ratio in the same way as in the SLIC paper
+    # so the values have the same meaning
+    step = float(max((step_z, step_y, step_x)))
+    ratio = 1.0 / compactness
+
+    image = np.ascontiguousarray(image * ratio)
+
+    labels = _slic_cython(image, segments, step, max_iter, spacing)
+
+    if enforce_connectivity:
+        segment_size = depth * height * width / n_segments
+        min_size = int(min_size_factor * segment_size)
+        max_size = int(max_size_factor * segment_size)
+        labels = _enforce_label_connectivity_cython(labels,
+                                                    n_segments,
+                                                    min_size,
+                                                    max_size)
+
+    if is_2d:
+        labels = labels[0]
+
+    return labels

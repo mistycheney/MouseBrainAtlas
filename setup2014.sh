83f3bca34ba0529779d7ab95bfcac3cbebd2d42b
Yuncong Chen <cyc3700@gmail.com>
2015-05-27 04:20:11 -0700

    sync

--- setup2014.sh
+++ setup2014.sh
@@ -5,7 +5,7 @@ export LD_LIBRARY_PATH=/opt/python/lib:$LD_LIBRARY_PATH:$CSD181/KDU74_Demo_Apps_
 export PATH=/oasis/projects/nsf/csd181/yuncong/virtualenv-1.9.1/yuncongve/bin:$PATH:$CSD181/KDU74_Demo_Apps_for_Linux-x86-64_140513
 #source /opt/intel/composer_xe_2013_sp1.2.144/bin/compilervars.sh intel64
 
-export GORDON_SECTIONDATA_DIR=$CSD181/DavidData2014sections
+export GORDON_DATA_DIR=$CSD181/DavidData2014tif
 export GORDON_REPO_DIR=$HOME/Brain
 export GORDON_RESULT_DIR=$CSD181/DavidData2014results
 export GORDON_LABELING_DIR=$CSD181/DavidData2014labelings

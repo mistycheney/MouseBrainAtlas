8aa5ba007bec66da6a8410574f05d22621fd29d7
Yuncong Chen <cyc3700@gmail.com>
2016-08-03 22:05:56 -0700

    directly resection volume in memory

--- gui/rectification_tool.py
+++ gui/rectification_tool.py
@@ -16,6 +16,14 @@ else:
 
 from ui_RectificationTool import Ui_RectificationGUI
 
+import cPickle as pickle
+
+import sys, os
+sys.path.append(os.environ['REPO_DIR'] + '/utilities')
+from utilities2015 import *
+from metadata import *
+from data_manager import *
+
 PEN_WIDTH = 2
 
 red_pen = QPen(Qt.red)
@@ -35,6 +43,10 @@ class RectificationTool(QMainWindow, Ui_RectificationGUI):
         # self.sagittal_gscene = QGraphicsScene(self.sagittal_gview)
 
         self.stack = stack
+        # self.volume = bp.unpack_ndarray_file(volume_dir + '/%(stack)s/%(stack)s_thumbnailVolume.bp' % {'stack':stack})
+        self.volume = bp.unpack_ndarray_file(volume_dir + '/%(stack)s/%(stack)s_down4Volume.bp' % {'stack':stack})
+        # self.volume = bp.unpack_ndarray_file(volume_dir + '/%(stack)s/%(stack)s_down8Volume.bp' % {'stack':stack})
+        self.y_dim, self.x_dim, self.z_dim = self.volume.shape
 
         self.coronal_gscene = QGraphicsScene()
         self.coronal_gview.setScene(self.coronal_gscene)
@@ -45,14 +57,13 @@ class RectificationTool(QMainWindow, Ui_RectificationGUI):
         self.sagittal_gscene = QGraphicsScene()
         self.sagittal_gview.setScene(self.sagittal_gscene)
 
-        # self.sagittal_gview.update(0, 0, self.sagittal_gview.width(), self.sagittal_gview.height())
-
-        # self.sagittal_gview.show()
 
         self.coronal_gscene.installEventFilter(self)
         self.horizontal_gscene.installEventFilter(self)
+        self.sagittal_gscene.installEventFilter(self)
         self.coronal_gview.viewport().installEventFilter(self)
         self.horizontal_gview.viewport().installEventFilter(self)
+        self.sagittal_gview.viewport().installEventFilter(self)
 
         # self.sagittal_gview.viewport().installEventFilter(self)
 
@@ -77,11 +88,23 @@ class RectificationTool(QMainWindow, Ui_RectificationGUI):
         self.horizontal_vline = QGraphicsLineItem()
         self.horizontal_vline.setPen(red_pen)
         # self.horizontal_vline.setZValue(0)
+        self.sagittal_hline = QGraphicsLineItem()
+        self.sagittal_hline.setPen(red_pen)
+        # self.horizontal_hline.setZValue(0)
+        self.sagittal_vline = QGraphicsLineItem()
+        self.sagittal_vline.setPen(red_pen)
+        # self.horizontal_vline.setZValue(0)
 
         self.coronal_gscene.addItem(self.coronal_hline)
         self.coronal_gscene.addItem(self.coronal_vline)
         self.horizontal_gscene.addItem(self.horizontal_hline)
         self.horizontal_gscene.addItem(self.horizontal_vline)
+        self.sagittal_gscene.addItem(self.sagittal_hline)
+        self.sagittal_gscene.addItem(self.sagittal_vline)
+
+        # self.x_dim = 99
+        # self.y_dim = 99
+        # self.z_dim = 99
 
         # self.sagittal_pixmap = QPixmap()
         # self.sagittal_pixmapItem = self.sagittal_gscene.addPixmap(self.sagittal_pixmap)
@@ -96,7 +119,6 @@ class RectificationTool(QMainWindow, Ui_RectificationGUI):
         # self.horizontal_pixmapItem.setZValue(-10)
 
 
-
         # self.sagittal_hline = self.coronal_gscene.addLine(0, self.y, self.coronal_gscene.width(), self.y)
         # self.sagittal_vline = self.coronal_gscene.addLine(0, self.y, self.coronal_gscene.width(), self.y)
 
@@ -104,11 +126,27 @@ class RectificationTool(QMainWindow, Ui_RectificationGUI):
         self.button_sameY.clicked.connect(self.sameY_clicked)
         self.button_sameZ.clicked.connect(self.sameZ_clicked)
 
+        self.slider_hxy.valueChanged.connect(self.slider_hxy_changed)
+        self.slider_hxz.valueChanged.connect(self.slider_hxz_changed)
+        self.slider_hyx.valueChanged.connect(self.slider_hyx_changed)
+        self.slider_hyz.valueChanged.connect(self.slider_hyz_changed)
+        self.slider_hzx.valueChanged.connect(self.slider_hzx_changed)
+        self.slider_hzy.valueChanged.connect(self.slider_hzy_changed)
+
+        self.button_done.clicked.connect(self.done_clicked)
+
         self.first_point_done = False
         self.mode = None
         self.recorded_pairs = {'sameX': [], 'sameY': [], 'sameZ': []}
 
         self.update_cross(0,0,0)
+
+        self.hxy = 0
+        self.hxz = 0
+        self.hyx = 0
+        self.hyz = 0
+        self.hzx = 0
+        self.hzy = 0
 
     def update_crosslines(self):
         print self.cross_x, self.cross_y, self.cross_z
@@ -116,10 +154,11 @@ class RectificationTool(QMainWindow, Ui_RectificationGUI):
         self.coronal_vline.setLine(self.z_dim-1-self.cross_z, 0, self.z_dim-1-self.cross_z, self.y_dim-1)
         self.horizontal_hline.setLine(0, self.cross_x, self.z_dim-1, self.cross_x)
         self.horizontal_vline.setLine(self.z_dim-1-self.cross_z, 0, self.z_dim-1-self.cross_z, self.x_dim-1)
-        # self.coronal_gscene.update(0, self.coronal_gscene.width(), 0, self.coronal_gscene.height())
-        # self.horizontal_gscene.update(0, self.horizontal_gscene.width(), 0, self.horizontal_gscene.height())
-        # self.coronal_gview.update()
-        # self.horizontal_gview.update()
+        self.sagittal_hline.setLine(0, self.cross_y, self.x_dim-1, self.cross_y)
+        self.sagittal_vline.setLine(self.cross_x, 0, self.cross_x, self.y_dim-1)
+
+    def done_clicked(self):
+        pickle.dump(self.recorded_pairs, open('/home/yuncong/CSHL_volumes_resection/recorded_pairs.pkl', 'w'))
 
     def sameX_clicked(self):
         self.mode = 'sameX'
@@ -132,35 +171,125 @@ class RectificationTool(QMainWindow, Ui_RectificationGUI):
 
     def update_gscenes(self, which):
 
+        gray_color_table = [qRgb(i, i, i) for i in range(256)]
+
         if which == 'y':
-            horizontal_fn = '/home/yuncong/CSHL_volumes_resection/horizontal/%(stack)s_thumbnailVolume_horizontal_%(y)03d.tif' % {'stack': self.stack, 'y': self.cross_y}
-            self.horizontal_pixmap = QPixmap(horizontal_fn)
+
+            # color http://stackoverflow.com/questions/9794019/convert-numpy-array-to-pyside-qpixmap color comes out wrong
+            # gray https://gist.github.com/smex/5287589 works fine; Must specify bytesPerLine, see http://doc.qt.io/qt-4.8/qimage.html#QImage-6
+
+            self.horizontal_data = self.volume[self.cross_y, :, :].flatten()
+            self.horizontal_image = QImage(self.horizontal_data, self.z_dim, self.x_dim, self.z_dim, QImage.Format_Indexed8)
+            self.horizontal_image.setColorTable(gray_color_table)
+
+            # a = gray2rgb(self.volume[self.cross_y, :, :])
+            # self.horizontal_data = (255 << 24 | a[:,:,0] << 16 | a[:,:,1] << 8 | a[:,:,2]).flatten().copy() # pack RGB values
+            # self.horizontal_image = QImage(self.horizontal_data, self.z_dim, self.x_dim, QImage.Format_RGB32)
+
+            self.horizontal_pixmap = QPixmap.fromImage(self.horizontal_image)
+
+            # horizontal_fn = '/home/yuncong/CSHL_volumes_resection/horizontal/%(stack)s_thumbnailVolume_horizontal_%(y)03d.tif' % {'stack': self.stack, 'y': self.cross_y}
+            # self.horizontal_pixmap = QPixmap(horizontal_fn)
             self.horizontal_pixmapItem.setPixmap(self.horizontal_pixmap)
 
-            self.x_dim = self.horizontal_pixmap.height()
-            self.z_dim = self.horizontal_pixmap.width()
+            # self.x_dim = self.horizontal_pixmap.height()
+            # self.z_dim = self.horizontal_pixmap.width()
 
         elif which == 'x':
-            coronal_fn = '/home/yuncong/CSHL_volumes_resection/coronal/%(stack)s_thumbnailVolume_coronal_%(x)03d.tif' % {'stack': self.stack, 'x': self.cross_x}
-            self.coronal_pixmap = QPixmap(coronal_fn)
+            # coronal_fn = '/home/yuncong/CSHL_volumes_resection/coronal/%(stack)s_thumbnailVolume_coronal_%(x)03d.tif' % {'stack': self.stack, 'x': self.cross_x}
+            # self.coronal_pixmap = QPixmap(coronal_fn)
+
+            # a = gray2rgb(self.volume[:, self.cross_x, :])
+            # self.coronal_data = (255 << 24 | a[:,:,0] << 16 | a[:,:,1] << 8 | a[:,:,2]).flatten().copy() # pack RGB values
+            # self.coronal_image = QImage(self.coronal_data, self.z_dim, self.y_dim, QImage.Format_RGB32)
+
+            self.coronal_data = self.volume[:, self.cross_x, :].flatten()
+            self.coronal_image = QImage(self.coronal_data, self.z_dim, self.y_dim, self.z_dim, QImage.Format_Indexed8)
+            self.coronal_image.setColorTable(gray_color_table)
+
+            self.coronal_pixmap = QPixmap.fromImage(self.coronal_image)
             self.coronal_pixmapItem.setPixmap(self.coronal_pixmap)
 
-            self.z_dim = self.coronal_pixmap.width()
-            self.y_dim = self.coronal_pixmap.height()
+            # self.z_dim = self.coronal_pixmap.width()
+            # self.y_dim = self.coronal_pixmap.height()
+
+        elif which == 'z':
+            # sagittal_fn = '/home/yuncong/CSHL_volumes_resection/sagittal/%(stack)s_thumbnailVolume_sagittal_%(z)03d.tif' % {'stack': self.stack, 'z': self.cross_z}
+            # self.sagittal_pixmap = QPixmap(sagittal_fn)
+
+            # a = gray2rgb(self.volume[:, :, self.cross_z])
+            # self.sagittal_data = (255 << 24 | a[:,:,0] << 16 | a[:,:,1] << 8 | a[:,:,2]).flatten().copy() # pack RGB values
+            # self.sagittal_image = QImage(self.sagittal_data, self.x_dim, self.y_dim, QImage.Format_RGB32)
+
+            self.sagittal_data = self.volume[:, :, self.cross_z].flatten()
+            self.sagittal_image = QImage(self.sagittal_data, self.x_dim, self.y_dim, self.x_dim, QImage.Format_Indexed8)
+            self.sagittal_image.setColorTable(gray_color_table)
+
+            self.sagittal_pixmap = QPixmap.fromImage(self.sagittal_image)
+            self.sagittal_pixmapItem.setPixmap(self.sagittal_pixmap)
+
+            # self.x_dim = self.sagittal_pixmap.width()
+            # self.y_dim = self.sagittal_pixmap.height()
+
 
     def update_cross(self, x=None, y=None, z=None):
 
         if y is not None:
-            self.cross_y = y
+            self.cross_y = max(0, min(self.y_dim-1, y))
             self.update_gscenes('y')
         if x is not None:
-            self.cross_x = x
+            self.cross_x = max(0, min(self.x_dim-1, x))
             self.update_gscenes('x')
         if z is not None:
-            self.cross_z = z
+            self.cross_z = max(0, min(self.z_dim-1, z))
             self.update_gscenes('z')
 
         self.update_crosslines()
+
+    def translate_gsceneCoord_to_3d(self, which, gscene_x, gscene_y):
+        if which == 'coronal':
+            x = self.cross_x
+            y = gscene_y
+            z = self.z_dim - 1 - gscene_x
+        elif which == 'horizontal':
+            x = gscene_y
+            y = self.cross_y
+            z = self.z_dim - 1 - gscene_x
+        elif which == 'sagittal':
+            x = gscene_x
+            y = gscene_y
+            z = self.cross_z
+
+        return (x,y,z)
+
+    def slider_hxy_changed(self, val):
+        self.hxy = val / 10.
+        self.update_transform()
+
+    def slider_hxz_changed(self, val):
+        self.hxz = val / 10.
+        self.update_transform()
+
+    def slider_hyx_changed(self, val):
+        self.hyx = val / 10.
+        self.update_transform()
+
+    def slider_hyz_changed(self, val):
+        self.hyz = val / 10.
+        self.update_transform()
+
+    def slider_hzx_changed(self, val):
+        self.hzx = val / 10.
+        self.update_transform()
+
+    def slider_hzy_changed(self, val):
+        self.hzy = val / 10.
+        self.update_transform()
+
+    def update_transform(self):
+        T = np.array([[1, self.hxy, self.hxz], [self.hyx, 1, self.hyz], [self.hzx, self.hzy, 1]])
+
+
 
     def eventFilter(self, obj, event):
 
@@ -170,18 +299,18 @@ class RectificationTool(QMainWindow, Ui_RectificationGUI):
 
             if event.delta() < 0: # negative means towards user
                 if obj == self.sagittal_gscene or obj == self.sagittal_gview.viewport():
-                    self.update_cross(z=min(self.cross_z+1, self.z_dim-1))
+                    self.update_cross(z=self.cross_z+1)
                 elif obj == self.coronal_gscene or obj == self.coronal_gview.viewport():
-                    self.update_cross(x=min(self.cross_x+1, self.x_dim-1))
+                    self.update_cross(x=self.cross_x+1)
                 elif obj == self.horizontal_gscene or obj == self.horizontal_gview.viewport():
-                    self.update_cross(y=min(self.cross_y+1, self.y_dim-1))
+                    self.update_cross(y=self.cross_y+1)
             else: # scroll away from user
                 if obj == self.sagittal_gscene or obj == self.sagittal_gview.viewport():
-                    self.update_cross(z=max(self.cross_z-1, 0))
+                    self.update_cross(z=self.cross_z-1)
                 elif obj == self.coronal_gscene or obj == self.coronal_gview.viewport():
-                    self.update_cross(x=max(self.cross_x-1, 0))
+                    self.update_cross(x=self.cross_x-1)
                 elif obj == self.horizontal_gscene or obj == self.horizontal_gview.viewport():
-                    self.update_cross(y=max(self.cross_y-1, 0))
+                    self.update_cross(y=self.cross_y-1)
 
             return True
 
@@ -193,7 +322,8 @@ class RectificationTool(QMainWindow, Ui_RectificationGUI):
             if self.mode is None:
 
                 if obj == self.sagittal_gscene:
-                    pass
+                    x, y, z = self.translate_gsceneCoord_to_3d('sagittal', gscene_x, gscene_y)
+                    self.update_cross(x, y, z)
                 elif obj == self.coronal_gscene:
                     self.update_cross(z=self.z_dim - 1 - gscene_x, y=gscene_y)
                 elif obj == self.horizontal_gscene:
@@ -203,37 +333,23 @@ class RectificationTool(QMainWindow, Ui_RectificationGUI):
             else:
 
                 if obj == self.sagittal_gscene:
-                    return True
+                    x, y, z = self.translate_gsceneCoord_to_3d('sagittal', gscene_x, gscene_y)
                 elif obj == self.coronal_gscene:
-                    x = self.cross_x
-                    y = gscene_y
-                    z = -gscene_x
-                    if not self.first_point_done:
-                        self.first_point = (x,y,z)
-                        print 'first point', self.first_point
-                        self.first_point_done = True
-                    else:
-                        self.second_point = (x,y,z)
-                        print 'second point', self.second_point
-                        self.first_point_done = False
-                        self.recorded_pairs[self.mode].append((self.first_point, self.second_point))
-                        self.mode = None
-                    return True
+                    x, y, z = self.translate_gsceneCoord_to_3d('coronal', gscene_x, gscene_y)
                 elif obj == self.horizontal_gscene:
-                    x = -gscene_y
-                    y = self.cross_y
-                    z = -gscene_x
-                    if not self.first_point_done:
-                        self.first_point = (x,y,z)
-                        print 'first point', self.first_point
-                        self.first_point_done = True
-                    else:
-                        self.second_point = (x,y,z)
-                        print 'second point', self.second_point
-                        self.first_point_done = False
-                        self.recorded_pairs[self.mode].append((self.first_point, self.second_point))
-                        self.mode = None
-                    return True
+                    x, y, z = self.translate_gsceneCoord_to_3d('horizontal', gscene_x, gscene_y)
+
+                if not self.first_point_done:
+                    self.first_point = (x,y,z)
+                    print 'first point', self.first_point
+                    self.first_point_done = True
+                else:
+                    self.second_point = (x,y,z)
+                    print 'second point', self.second_point
+                    self.first_point_done = False
+                    self.recorded_pairs[self.mode].append((self.first_point, self.second_point))
+                    self.mode = None
+                return True
 
         return False
 

284b195e608cfc2f6fe703ca0af3bb6d65fd6be4
Saienthan <saienthan@gmail.com>
2016-11-19 06:40:01 +0000

    sync

--- learning/TEMPpipeline_test.py
+++ learning/TEMPpipeline_test.py
@@ -7,14 +7,17 @@ from data_manager import *
 from metadata import *
 # Align
 all_stacks = ['MD602', 'MD603']
+# Transform
+
 t = time.time()
-sys.stderr.write('align all subjects to atlas ...')
+sys.stderr.write('transform atlas ...')
+
 exclude_nodes = [33]
 
-run_distributed_aws(command='%(script_path)s %%(stack)s 1 1 atlasV2' % \
-                {'script_path': os.path.join(os.environ['REPO_DIR'], 'registration') + '/align_subject_brain_to_atlas_v2.py'},
+run_distributed_aws(command='%(script_path)s %%(stack)s 1 1 0 atlasV2' % \
+                {'script_path': os.path.join(os.environ['REPO_DIR'], 'registration') + '/transform_brains.py'},
                 kwargs_list=dict(stack=all_stacks),
                 exclude_nodes=exclude_nodes,
                 argument_type='single')
 
-sys.stderr.write('done in %f seconds\n' % (time.time() - t))  # 526 seconds
+sys.stderr.write('done in %f seconds\n' % (time.time() - t)) # 310 seconds

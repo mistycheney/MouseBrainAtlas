1b727b7b266ce6c067a3ac4e3dea9c822be14470
Yuncong Chen <cyc3700@gmail.com>
2016-12-19 19:46:38 -0800

    add MD635 into all_stacks

--- utilities/metadata.py
+++ utilities/metadata.py
@@ -136,7 +136,8 @@ xy_pixel_distance_tb = xy_pixel_distance_lossless * 32 # in um, thumbnail
 
 #######################################
 
-all_stacks = ['MD585', 'MD589', 'MD590', 'MD591', 'MD592', 'MD593', 'MD594', 'MD595', 'MD598', 'MD599', 'MD602', 'MD603']
+all_stacks = ['MD585', 'MD589', 'MD590', 'MD591', 'MD592', 'MD593', 'MD594', 'MD595', 'MD598', 'MD599', 'MD602', 'MD603',
+'MD635']
 
 # section_number_lookup = { 'MD585': 440, 'MD589': 445,
 #                         'MD590': 419, 'MD591': 452, 'MD592': 454,

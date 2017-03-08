1438a058aac705ce86acd9426d0ef0e7b6ff1872
Yuncong Chen <cyc3700@gmail.com>
2017-03-08 00:57:14 -0800

    new finetune code

--- utilities/metadata.py
+++ utilities/metadata.py
@@ -84,7 +84,8 @@ elif hostname == 'yuncong-Precision-WorkStation-T7500':
     annotation_midbrainIncluded_v2_rootdir = '/home/yuncong/CSHL_labelings_v3/'
     # patch_features_rootdir = '/home/yuncong/CSHL_patch_features_Sat16ClassFinetuned_v2'
     PATCH_FEATURES_ROOTDIR = '/media/yuncong/BstemAtlasData/CSHL_patch_features'
-    ANNOTATION_ROOTDIR = None
+    ANNOTATION_ROOTDIR = '/home/yuncong/CSHL_labelings_v3/'
+    CLF_ROOTDIR = '/home/yuncong/CSHL_classifiers'
 else:
     print 'Setting environment for Brainstem workstation'
 
